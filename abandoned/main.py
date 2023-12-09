ff3f792f2cf710376f5cb40e7cc98203b6d2a552
aradhyasakalley <sakalleyaradhya@gmail.com>
2023-12-09 18:35:02 +0530

    Distance logic for abandoned object

--- abandoned/main.py
+++ abandoned/main.py
@@ -20,6 +20,13 @@ classes_to_track = ['person', 'handbag', 'suitcase']  # Define the classes to tr
 
 tracker = Tracker()  # Initialize the object tracker
 
+threshold_distance = 200  # Set a threshold distance to determine abandonment (adjust as needed)
+
+# Dictionary to store the initial positions of tracked bags/suitcases
+initial_positions = {}
+person_bag_association = {}
+abandoned_objects_count = 0
+counted_abandoned_objects = {}
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -58,13 +65,21 @@ while True:
                 class_label = objects_to_track[obj_id][4]
                 cv2.rectangle(frame, (x1, y1), (x2, y2), colors.get(class_label, (0, 0, 255)), 2)  # Use default color if class_label is not found in colors dictionary
 
-    # Calculate distances between centers of different objects
-    for i in range(len(tracked_objects)):
-        for j in range(i + 1, len(tracked_objects)):
-            x1, y1, _, _, _ = tracked_objects[i]
-            x2, y2, _, _, _ = tracked_objects[j]
-            dist = math.hypot((x2 - x1), (y2 - y1))
-            print(f"Distance between object {i} and object {j}: {dist}")
+                # Store initial positions of bags/suitcases
+                if class_label in ['handbag', 'suitcase'] and obj_id not in initial_positions:
+                    initial_positions[obj_id] = (x1, y1, x2, y2)
+
+    # Check for abandoned objects
+    for obj_id, initial_pos in initial_positions.items():
+        if obj_id < len(tracked_objects) and len(tracked_objects[obj_id]) >= 5:  # Check if obj_id is valid
+            current_pos = tracked_objects[obj_id][:4]
+            dist = math.hypot((current_pos[0] - initial_pos[0]), (current_pos[1] - initial_pos[1]))
+            if obj_id not in counted_abandoned_objects and dist > threshold_distance:
+                abandoned_objects_count += 1  # Increment the count
+                counted_abandoned_objects[obj_id] = True  # Mark the object as counted
+                print(f"Abandoned object detected: {tracked_objects[obj_id]}")
+    
+    cv2.putText(frame, f'Abandoned Objects: {abandoned_objects_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)      
 
     # cv2.polylines(frame, [np.array(area1, np.int32)], True, (255, 0, 0), 2)
     # cv2.putText(frame, str('1'), (504, 471), cv2.FONT_HERSHEY_COMPLEX, (0.5), (0, 0, 0), 1)
@@ -78,3 +93,6 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+print(f"Total abandoned objects detected: {abandoned_objects_count}")
